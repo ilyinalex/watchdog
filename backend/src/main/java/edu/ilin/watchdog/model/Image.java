514655daeadb476de7584cae7015a0cd449788cf
ilyinalex <ilinoleksiii@gmail.com>
2020-04-20 17:03:12 +0300

    add jpa, ImageRepository

--- backend/src/main/java/edu/ilin/watchdog/model/Image.java
+++ backend/src/main/java/edu/ilin/watchdog/model/Image.java
@@ -2,13 +2,26 @@ package edu.ilin.watchdog.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "images")
 @Getter
 @Setter
 public class Image {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private Integer id;
 
     private String name;
 
-    //TODO status, body...
+    @Lob
+    private byte[] data;
 }
---
+++ backend/src/main/java/edu/ilin/watchdog/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package edu.ilin.watchdog.repository;
+
+import edu.ilin.watchdog.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+}
