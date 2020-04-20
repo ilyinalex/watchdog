62050873d1b1340c2f38140e4e3a9abef2e93c4b
ilyinalex <ilinoleksiii@gmail.com>
2020-04-20 17:15:54 +0300

    add Model superclass

--- backend/src/main/java/edu/ilin/watchdog/model/Image.java
+++ backend/src/main/java/edu/ilin/watchdog/model/Image.java
@@ -1,27 +1,41 @@
 package edu.ilin.watchdog.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "images")
 @Getter
 @Setter
-public class Image {
+public class Image extends Model {
+
     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
     private String name;
 
     @Lob
     private byte[] data;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
 }
--- backend/src/main/java/edu/ilin/watchdog/model/User.java
+++ backend/src/main/java/edu/ilin/watchdog/model/User.java
@@ -1,12 +1,25 @@
 package edu.ilin.watchdog.model;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @Getter
 @Setter
-public class User {
-    private Integer id;
+@Entity
+@Table(name = "users")
+public class User extends Model {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
     private String name;
 }
